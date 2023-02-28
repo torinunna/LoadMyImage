d3c333249f470051c7282417f54a0f8d9bf8e03b
torinunna <torinunna9588@gmail.com>
2023-02-28 13:54:30 +0900

    Initial Commit

---
+++ LoadMyImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoadMyImage
+//
+//  Created by YUJIN KWON on 2023/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
